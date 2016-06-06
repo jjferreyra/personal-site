95b0a588e3ff0f673ed30dd210e2a14f2d3f57cf
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-06-07 00:13:50 +0100

    Main page animation finished (for now? Might improve later)

--- js/main.js
+++ js/main.js
@@ -2,10 +2,4 @@
 $(document).ready(function() {
     $('#top').hide().fadeIn(2000)
     $('#top-text').hide().show(2000)
-
-     $('#overlay').animate({
-       opacity: 1,
-     }, 5000, function() {
-        // Animation complete.
-     });
 });
