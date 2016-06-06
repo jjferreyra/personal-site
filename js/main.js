b92e6fdd5ad4b202da8950bcde40601a1931adf5
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-06-06 23:53:10 +0100

    Working on main page refresh animation

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-var pTags = document.getElementsByTagName('p')
-console.log(pTags)
+
+$(document).ready(function() {
+    $('#top').hide().fadeIn(2000)
+    $('#top-text').hide().show(2000)
+
+     $('#overlay').animate({
+       opacity: 1,
+     }, 5000, function() {
+        // Animation complete.
+     });
+});
