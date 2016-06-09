fe5e0f138ee00caea202d3cdae9e709c98f15bce
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-06-09 21:33:40 +0100

    Main page short beginning intro looks nice

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
-    $('main').hide()
-    
+    $('#main').hide()
+
     setTimeout(function() {
         $('#main').fadeIn(3000)
         $('#top').hide().slideDown(3000).fadeIn(3000)
-    }, 10000)
+    }, 5000)
 
 
 });
