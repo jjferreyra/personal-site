aeba72df9b6f8e25b8b5aa721c14c9216530d46f
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-06-09 21:15:43 +0100

    Seperated cool-intro in to a new module (will make another repo for it)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
 $(document).ready(function() {
-    $('#top').hide().fadeIn(2000)
+    $('main').hide()
+    
+    setTimeout(function() {
+        $('#main').fadeIn(3000)
+        $('#top').hide().slideDown(3000).fadeIn(3000)
+    }, 10000)
+
+
 });
